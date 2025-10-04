fa7861a95b2f6cf82c761a44a01ffd2d42a259c0
AngryCyboot <cyboot25@gmail.com>
2025-10-04 20:56:15 +0200

    tax setting

--- Scripts/Places/place.gd
+++ Scripts/Places/place.gd
@@ -10,7 +10,11 @@ enum Taxes { LOW, MEDIUM, HIGH }
 @export var foodStock: int = 0
 @export var population: int = 0
 @export var crimeRate: float = 0
-@export var taxeLevel: Taxes = Taxes.LOW
+@export var taxeLevel: int = Taxes.LOW :
+	set(x):
+		while x > Taxes.HIGH:
+			x -= 3
+		taxeLevel = x
 
 @export_group("Generation")
 @export var minGold: int = 0
@@ -26,6 +30,7 @@ var outgoing_roads : Array[Road]
 var hovered : bool = false
 var accessible : bool = false
 var current : bool = false
+var tax_hovered : bool = false
 
 func generatePlace() -> void:
 	goldStock = randi_range(minGold, maxGold) 
@@ -53,10 +58,13 @@ func _hover(x : bool = false):
 			tween.tween_property($CentralPuck,"position:y",0,0.2)
 
 func _input(event):
-	if hovered:
-		if event is InputEventMouseButton:
-			if event.button_index == MOUSE_BUTTON_LEFT:
+	
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if hovered:
 				GlobalEventHolder.turnEnd.emit(self)
+			elif tax_hovered:
+				_on_tax_changed()
 
 func _cart_accessibility(x : Place) :
 	if x == self:
@@ -70,3 +78,12 @@ func _turn_end():
 	current = false
 	accessible = false
 	hovered = false
+
+func tax_hover(x:bool=false):
+	tax_hovered = x
+	$Sprite3D/SubViewport/MarginContainer/Panel2.visible = x
+
+func _on_tax_changed():
+	taxeLevel += 1
+	$Sprite3D/SubViewport/MarginContainer/VBoxContainer/TaxLevel2.visible = taxeLevel >= Taxes.MEDIUM
+	$Sprite3D/SubViewport/MarginContainer/VBoxContainer/TaxLevel.visible = taxeLevel == Taxes.HIGH
