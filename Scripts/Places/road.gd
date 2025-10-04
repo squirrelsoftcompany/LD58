9efe17da998a28086ab3adb0e7269a16782fb594
AngryCyboot <cyboot25@gmail.com>
2025-10-04 15:06:26 +0200

    road cost ui

--- Scripts/Places/road.gd
+++ Scripts/Places/road.gd
@@ -1,6 +1,6 @@
 @tool
 extends Node3D
-
+@export var text_altitude : float = 5
 @export var cobbled : bool = false
 @export var cost : int = 1
 @export var crimerate : float = 0 ## percentage
@@ -25,10 +25,13 @@ func _ready():
 	innit()
 
 func _process(_delta):
-	if Engine.is_editor_hint():
+	if Engine.is_editor_hint() and from != null and to != null:
 		innit()
 
 func innit():
 	var path = $Path3D
 	path.curve.set_point_position(0,from.position)
 	path.curve.set_point_position(1,to.position)
+	$Sprite3D.position = ((to.position-from.position)/2)+from.position
+	$Sprite3D.position.y = text_altitude
+	$Sprite3D/SubViewport/Label.text = cost
