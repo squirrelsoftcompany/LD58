a6518a0df779e02da62d151998812dd34df81b58
AngryCyboot <cyboot25@gmail.com>
2025-10-05 14:15:05 +0200

    coravan gotta collect

--- Scripts/caravan.gd
+++ Scripts/caravan.gd
@@ -3,6 +3,7 @@ extends Node3D
 var current_zone : Place
 var next_zone : Place
 var movement_tween : Tween
+var gold : int = 0
 
 @export var capital : Node3D
 @export var food : int = 10
@@ -11,15 +12,19 @@ func _ready():
 	position = capital.position
 	GlobalEventHolder.turnEnd.connect(_move)
 	GlobalEventHolder.loopStart.emit() ## TODO game bootup sequence
+	print("TODO: caravan still starts the loop")
 
 func _move(x:Place):
 	next_zone = x
+	look_at(next_zone.position)
 	movement_tween = create_tween()
 	movement_tween.tween_property(self,"position",next_zone.position,0.5)
 	movement_tween.tween_callback(_move_done)
 
 func _move_done():
 	current_zone = next_zone
+	gold += current_zone.taxable
+	print("TODO: show gold collected, it's ",gold," btw")
 	if current_zone == capital :
 		GlobalEventHolder.loopEnd.emit()
 	else:
