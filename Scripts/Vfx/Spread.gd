fc26c3ded0a915e1c99540a6780e5c548e9c6efd
AngryCyboot <cyboot25@gmail.com>
2025-10-05 21:04:50 +0200

    spiking

---
+++ Scripts/Vfx/Spread.gd
@@ -0,0 +1,13 @@
+extends MultiMeshInstance3D
+
+@export var radial_orientation : bool = false
+@export var angle : float = 0 ## if radial orientation true
+@export var nb_max : int = 10
+@export var watched_variable : String
+@export var min_range : float
+@export var max_range : float
+
+var meshes : Array
+
+func _ready():
+	
