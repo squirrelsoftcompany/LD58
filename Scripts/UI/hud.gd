94bc79dacde7caba5e23d8850c98ec8236100d4d
AngryCyboot <cyboot25@gmail.com>
2025-10-06 19:49:24 +0200

    Heads Up Planks

---
+++ Scripts/UI/hud.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@export var food_label : Label
+@export var food_max_label : Label
+@export var gold_label : Label
+@export var gold_goal_label : Label
+
+var caravan : Caravan
+
+func _ready():
+	caravan = get_tree().get_first_node_in_group("caravan")
+	GlobalEventHolder.turnStart.connect(func(_x): _update_gold())
+	GlobalEventHolder.turnEnd.connect(func(_x): _update_food())
+	GlobalEventHolder.reward_received.connect(func(_x): _update_all())
+	_update_all()
+
+func _update_all():
+	_update_food()
+	_update_gold()
+
+func _update_gold():
+	gold_label.text = str(caravan.gold)
+
+func _update_food():
+	food_label.text = str(caravan.food)
