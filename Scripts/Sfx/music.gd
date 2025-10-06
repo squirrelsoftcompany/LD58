c1a8c5acb260560b5924f7d3c5c8f96049bb6666
AngryCyboot <cyboot25@gmail.com>
2025-10-06 23:32:24 +0200

    music setup

---
+++ Scripts/Sfx/music.gd
@@ -0,0 +1,13 @@
+extends AudioStreamPlayer3D
+
+@onready var music : AudioStreamPlaybackInteractive = self.get_stream_playback()
+
+func _ready():
+	GlobalEventHolder.startAmbush.connect(_ambush_in)
+	GlobalEventHolder.endAmbush.connect(_ambush_out)
+
+func _ambush_in():
+	music.switch_to_clip(1)
+
+func _ambush_out():
+	music.switch_to_clip(0)
