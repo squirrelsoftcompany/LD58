89d6fe8980855d46ae77bf37ae738caf25d6884d
AngryCyboot <cyboot25@gmail.com>
2025-10-19 19:14:46 +0200

    fix absolutely complete reinnit on gameover

--- giant_spaghetti_monster.gd
+++ giant_spaghetti_monster.gd
@@ -2,10 +2,18 @@ extends Node3D
 
 @export var map : PackedScene 
 
+var cycling : bool = false
+
 func _ready():
 	GlobalEventHolder.gameOver.connect(game_over)
 	add_child(map.instantiate())
 
+func _process(_delta):
+	if cycling:
+		add_child(map.instantiate())
+		cycling = false
+
 func game_over():
 	get_child(0).queue_free()
-	add_child(map.instantiate())
+	cycling = true
+	
